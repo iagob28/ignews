51cf68ab384cfcb2ea3683818bfaebae0aa1f446
iagob28 <61669995+iagob28@users.noreply.github.com>
2022-03-11 14:09:16 -0300

    Header and basic design created

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 monthly</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl codign" />
+      </main>
+    </>
+  );
+}
