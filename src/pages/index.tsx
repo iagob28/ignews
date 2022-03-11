1cb8abc9bf834fea8cfb3b49456e8f3c79fd35e3
iagob28 <61669995+iagob28@users.noreply.github.com>
2022-03-11 15:32:48 -0300

    static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -10,6 +10,10 @@ interface HomeProps {
     amount: number;
   };
 }
+
+// Cliente side
+// Server side
+// and Static Site Genaration
 
 export default function Home({ product }: HomeProps) {
   return (
@@ -35,8 +39,9 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KcCZ8HjRXPcj4pX5npp5VLn");
+
   const product = {
     priceId: price.id,
     amount: new Intl.NumberFormat("en-US", {
@@ -46,5 +51,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
   };
   return {
     props: { product },
+    revalidate: 60 * 60 * 24, // 24 Hours
   };
 };
