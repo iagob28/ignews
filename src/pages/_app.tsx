cc093247d49a8d7d1a33a6b90064ea4ef9854656
iagob28 <61669995+iagob28@users.noreply.github.com>
2022-03-12 16:04:13 -0300

    Auth with github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
+import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 
 import "../styles/global.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <SessionProvider session={session} refetchInterval={5 * 60}> {/* 5 minutes */}
+        <Header />
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }
