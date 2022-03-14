070116066b3ea63a25af2c89ac09ba7369398545
iagob28 <61669995+iagob28@users.noreply.github.com>
2022-03-14 16:52:27 -0300

    Purchase method applied

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,9 +8,35 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    } else {
+      try {
+        const response = await api.post("/subscribe");
+        console.log(response.data);
+        const { sessionId } = response.data;
+
+        const stripe = await getStripeJs();
+        console.log(stripe);
+
+        await stripe.redirectToCheckout({ sessionId });
+      } catch (error) {
+        alert(error.message);
+      }
+    }
+  }
+
   return (
     <>
-      <button type="button" className={styles.subscribeButton}>
+      <button
+        type="button"
+        className={styles.subscribeButton}
+        onClick={handleSubscribe}
+      >
         Subscribe Now
       </button>
     </>
