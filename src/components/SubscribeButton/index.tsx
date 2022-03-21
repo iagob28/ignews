ebd432a599a6fee0c9c83b9954af3cd0fefe4dd3
iagob28 <61669995+iagob28@users.noreply.github.com>
2022-03-21 16:16:02 -0300

    Preview pages

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { api } from "../../services/api";
 import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
@@ -9,23 +10,28 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
-
+  const router = useRouter();
   async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
-    } else {
-      try {
-        const response = await api.post("/subscribe");
-        
-        const { sessionId } = response.data;
+    }
 
-        const stripe = await getStripeJs();
+    if (session.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
 
-        await stripe.redirectToCheckout({ sessionId });
-      } catch (error) {
-        alert(error.message);
-      }
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
     }
   }
 
